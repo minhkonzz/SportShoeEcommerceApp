5818e4a0edc45c938bef15845072a9f97211a753
minhtri16 <triduc201@gmail.com>
2022-05-31 23:10:53 +0700

    Don't have the product detail in the screen

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Main } from './screens/Main';
 import { Auth } from './screens/Auth';
-import { ProductDetail } from './screens/ProductDetail';
+//import { ProductDetail } from './screens/ProductDetail';
 import { OrderTracking } from './screens/OrderTracking';
 import { Cart } from './screens/Cart';
 
@@ -18,7 +18,6 @@ const App = () => (
       }}>
       <Stack.Screen name='Main' component={Main}/>
       <Stack.Screen name='Auth' component={Auth}/>
-      <Stack.Screen name='ProductDetail' component={ProductDetail}/>
       <Stack.Screen name='OrderTracking' component={OrderTracking}/>
       <Stack.Screen name='Cart' component={Cart}/>
     </Stack.Navigator>
